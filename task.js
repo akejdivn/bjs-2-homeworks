3782ba2d87b324b7366e4023b5d0438fc0d99a57
Alena <murawievaa@yandex.ru>
2024-04-05 16:40:42 +0300

    проверка первой задачи

---
+++ task.js
@@ -0,0 +1,23 @@
+"use strict"
+
+function solveEquation(a, b, c) {
+	let arr = [];
+	let discriminant = b ** 2 - 4 * a * c;
+	if (discriminant < 0) {
+		arr = []
+	} else if (discriminant === 0) {
+		arr = [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)]
+	} else {
+		arr = [-b / (2 * a)]
+	}
+	return arr;
+};
+
+
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+
+	percent = Math.round(Math.random());
+	loanBody = amount - contribution;
+	countMonths = loanBody * ((1 / 12) * percent + ((1 / 12) * percent / Math.pow(((1 + (1 / 12) * percent), countMonths) - 1)))
+}
+calculateTotalMortgage(10, 0, 50000)
