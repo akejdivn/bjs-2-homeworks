01db55de7eb79dd75c9c53a9c56fe5bcd17a7d7b
Alena <murawievaa@yandex.ru>
2024-04-08 19:48:55 +0300

    initial commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,26 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
-}
+	let arr = [];
+	let discriminant = b ** 2 - 4 * a * c;
+	if (discriminant < 0) {
+		arr = []
+	} else if (discriminant === 0) {
+		arr.push(...(-b + Math.sqrt(discriminant)) / (2 * a), (-b - Math.sqrt(discriminant)) / (2 * a))
+
+	} else {
+		arr = [-b / (2 * a)]
+	}
+	return arr;
+
+};
+
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+
+	percent = Math.round(Math.random());
+	let loanBody = amount - contribution;
+	countMonths = loanBody * ((1 / 12) * percent + ((1 / 12) * percent / Math.pow(((1 + (1 / 12) * percent), countMonths) - 1)))
+	allLoan = MAth.ron((loanBody * percent) * countMonths)
+	console.log(allLoan)
 }
