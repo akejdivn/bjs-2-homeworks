4c8208785a3e341b7a6d665361dcebefc4749acf
Alena <murawievaa@yandex.ru>
2024-04-09 17:42:30 +0300

    форматирование

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,7 +1,19 @@
 function getArrayParams(...arr) {
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+    for (i = 0; i< arr.length; i++){
+      if (arr < min ){
+        max = arr;
+      } else if ( arr > max) {
+        min = arr;
+      }
+      avg =+ arr[i] / arr.length;
+  
+    }
+    return { min: min, max: max, avg: avg };
+  }
 
-  return { min: min, max: max, avg: avg };
-}
 
 function summElementsWorker(...arr) {
 
