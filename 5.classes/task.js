f2e2b4bbd8a7761c801d6e23e3eefc73a9ca18e6
Alena <murawievaa@yandex.ru>
2024-05-12 18:16:56 +0300

    внесение правок

---
+++ 5.classes/task.js
@@ -0,0 +1,92 @@
+class PrintEditionItem{
+    constructor(name, releaseDate, pagesCount, state = 100, type = null){
+ this.name = name;
+ this.releaseDate =releaseDate;
+ this.pagesCount = pagesCount;
+ this.state = state;
+ this.type = type;
+}
+fix() {
+    this.state *= 1.5; 
+    }
+
+  
+  set state(newState) {
+    
+  if (newState < 0) {
+      this.state = 0;
+  } else if (newState > 100) {
+    this.state = 100
+  } 
+      this._state = newState;
+  
+  }
+  get state() {
+    return this._state;
+  }
+}
+
+const sherlock = new PrintEditionItem(
+ "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе",
+ 2019,
+ 1008
+);
+
+
+class Magazine extends PrintEditionItem{
+  constructor(name, releaseDate, pagesCount, state, type = "magazine"){
+    super(name, releaseDate, pagesCount, state, type);
+  }
+}
+
+class Book extends PrintEditionItem{
+  constructor(author, name, releaseDate, pagesCount, state, type = "book"){
+    super(name, releaseDate, pagesCount, state, type);
+    this.author = author;
+  }
+}
+
+class NovelBook extends Book {
+  constructor(author, name, releaseDate, pagesCount, state, type = "novel"){
+    super(author, name, releaseDate, pagesCount, state, type);
+  }
+} 
+class FantasticBook extends Book{
+  constructor(author, name, releaseDate, pagesCount, state, type = "fantastic"){
+    super(author, name, releaseDate, pagesCount, state, type);
+  }
+}
+class DetectiveBook extends Book{
+    constructor(author, name, releaseDate, pagesCount, state, type = "detective"){
+      super(author, name, releaseDate, pagesCount, state, type);
+    }
+  }
+
+class Library {
+    constructor(name){
+    this.name = name,
+    this.books = []
+    }
+    addBook(book){
+        if(this.state > 30){
+            this.books.push(book)
+        }
+    }
+    findBookBy(type, value){
+      for(let i = 0; i < this.books.length; i++){
+        if(this.books[i][type] === value){
+          return this.books[i].name
+        }
+      }
+      return null
+      
+    }
+
+  giveBookByName(bookName){
+    if(this.books.includes(bookName)){
+      let deletBook = this.books.findIndex(bookName);
+      this.books.splice(deletBook, 1)
+    }
+    return null ;
+  }
+}
