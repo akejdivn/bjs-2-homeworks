57970e46b87fd2284bd4361ea974777fde9bf8b0
Alena <murawievaa@yandex.ru>
2024-05-27 16:41:14 +0300

    задачи выполнены

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,53 +1,50 @@
 ﻿function parseCount(quantity) {
-	let num = Number(quantity)
-	if (isNaN(num)) {
-		throw new Error("Невалидное значение")
-	} else {
-		return Number.parseFloat(num)
-	}
+ let num = Number(quantity)
+  if (isNaN(num)) {
+    throw new Error("Невалидное значение")
+  } else {
+	return Number.parseFloat(num)
+ }
 }
 
 function validateCount(quantity) {
-
-	try {
-		return parseCount(num)
-	} catch (error) {
-		return error
-	}
+ try {
+	return parseCount(num)
+ } catch (error) {
+	return error
+ }
 }
 
 class Triangle {
-	constructor(a, b, c) {
-		this.a = a;
-		this.b = b;
-		this.c = c
-		if ((a + b) < c || (a + c) < b || (b + c) < a) {
-			throw new Error("Треугольник с такими сторонами не существует")
-		}
-
-
-	}
-	get perimeter() {
-		return this.a + this.b + this.c
-	}
-	get area() {
-		let p = (this.a + this.b + this.c) * 0.5;
-		let s = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
-		return Number(s.toFixed(3))
+ constructor(a, b, c) {
+ this.a = a;
+ this.b = b;
+ this.c = c
+	if ((a + b) < c || (a + c) < b || (b + c) < a) {
+		throw new Error("Треугольник с такими сторонами не существует")
+ }
+ }
+ get perimeter() {
+	return this.a + this.b + this.c
+ }
+ get area() {
+	let p = (this.a + this.b + this.c) * 0.5;
+	let s = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+	return Number(s.toFixed(3))
 	}
 }
 
 function getTriangle(a, b, c) {
-	try {
-		return new Triangle(a, b, c);
-	} catch (error) {
-		return {
-			get area() {
-				return "Ошибка! Треугольник не существует"
-			},
-			get perimeter() {
-				return "Ошибка! Треугольник не существует"
-			}
-		}
+ try {
+	return new Triangle(a, b, c);
+  } catch (error) {
+	 return {
+	  get area() {
+	  return "Ошибка! Треугольник не существует"
+	  },
+     get perimeter() {
+     return "Ошибка! Треугольник не существует"
+      }
 	}
+ }
 }
